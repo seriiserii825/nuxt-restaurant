cc2ad1be9ac0a6cf83a624039a02032332bae548
serii burduja <seriiburduja@gmail.com>
2025-05-16 12:48:56 +0300

    feat: admin dashboard login page

---
+++ pages/admin/dashboard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "admin",
+});
+</script>
+
+<template>
+  <div class="dashboard">
+    <h3>Dashboard</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio fugiat alias qui nam dignissimos
+      dolorem, ab minus accusamus doloremque itaque illum recusandae, possimus hic nobis
+      perspiciatis culpa. Eligendi, ullam minus?
+    </p>
+  </div>
+</template>
--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: false,
+  layout: "admin",
 });
 </script>
 
