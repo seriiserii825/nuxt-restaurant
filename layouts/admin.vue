664b8504b7539f96900eb59afd74a45a84e46a6f
serii burduja <seriiburduja@gmail.com>
2025-05-18 13:39:53 +0300

    feat: admin header

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,12 +1,24 @@
 <script lang="ts" setup>
 useHead({
   link: [
-    { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" },
+    {
+      rel: "stylesheet",
+      href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+    },
+    {
+      rel: "stylesheet",
+      href: "/backend/assets/css/icons.min.css",
+    },
+    {
+      rel: "stylesheet",
+      href: "/backend/assets/css/app.min.css",
+    }
   ],
 });
 </script>
 <template>
   <div class="admin-layout">
+    <AdminHeader />
     <slot />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@ onMounted(() => {
 <template>
   <div class="home">
     <h1>Home</h1>
+    <a href="/admin/login" class="btn btn-success">Login</a>
     <a href="#" @click.prevent="useLogout" class="btn btn-danger">Logout</a>
   </div>
 </template>
